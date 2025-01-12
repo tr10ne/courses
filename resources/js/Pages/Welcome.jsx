c0d93e477c73e759c7a4b1d80fc855450b6b0591
Ivan Trifonov <tr1one@ya.ru>
2025-01-12 17:55:52 +0300

    big inter

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -51,7 +51,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                             href={route("login")}
                                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Вход
+                                            ВХОД
                                         </Link>
                                         <Link
                                             href={route("register")}
