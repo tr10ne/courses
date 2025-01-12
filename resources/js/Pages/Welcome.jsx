2121cdf48eb314331c7bc1224c2dc723883ca55d
Xander White <xander.belov23@yandex.ru>
2025-01-12 17:58:59 +0300

    little dick

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -57,7 +57,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                             href={route("register")}
                                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            РЕГИСТРАЦИЯ ХУЙ
+                                            РЕГИСТРАЦИЯ хуй
                                         </Link>
                                     </>
                                 )}
