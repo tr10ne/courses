eebae31d63a3810c8b4ad920b494be7ba85cc105
Ivan Trifonov <tr1one@ya.ru>
2025-02-18 16:56:11 +0300

    add logic ReviewsScore

--- frontend/src/Components/SchoolReviews/ReviewsScore.jsx
+++ frontend/src/Components/SchoolReviews/ReviewsScore.jsx
@@ -15,8 +15,23 @@ const ReviewsScore = ({
     return total > 0 ? ((count / total) * 100).toFixed(2) : 0;
   };
 
-  const ratings = { rating5, rating4, rating3, rating2, rating1 };
-  const maxRating = Math.max(...Object.values(ratings));
+  const ratings = [
+    { rating: 5, count: rating5 },
+    { rating: 4, count: rating4 },
+    { rating: 3, count: rating3 },
+    { rating: 2, count: rating2 },
+    { rating: 1, count: rating1 },
+  ];
+
+  const maxCount = Math.max(...ratings.map((r) => r.count));
+
+  const maxRatings = ratings.filter((r) => r.count === maxCount);
+
+  const highestRating = maxRatings.reduce((prev, current) =>
+    prev.rating > current.rating ? prev : current
+  );
+
+  const hasReviews = totalReviews > 0;
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -33,80 +48,31 @@ const ReviewsScore = ({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div
-        className={`reviews-score__item ${
-          rating5 === maxRating && !isHovered ? "highlighted" : ""
-        }`}
-      >
-        <p className="item-score">5 звезд</p>
-        <div className="progress-bar">
-          <p className="item-value">{rating5} отзывов</p>
-          <div
-            className="progress"
-            style={{ width: `${calculatePercentage(rating5, totalReviews)}%` }}
-          ></div>
+      {ratings.map(({ rating, count }) => (
+        <div
+          key={rating}
+          className={`reviews-score__item ${
+            hasReviews &&
+            count === highestRating.count &&
+            rating === highestRating.rating &&
+            !isHovered
+              ? "highlighted"
+              : ""
+          }`}
+        >
+          <p className="item-score">
+            {rating} звезд
+            {rating === 1 ? "" : rating >= 2 && rating <= 4 ? "ы" : ""}
+          </p>
+          <div className="progress-bar">
+            <p className="item-value">{count} отзывов</p>
+            <div
+              className="progress"
+              style={{ width: `${calculatePercentage(count, totalReviews)}%` }}
+            ></div>
+          </div>
         </div>
-      </div>
-
-      <div
-        className={`reviews-score__item ${
-          rating4 === maxRating && !isHovered ? "highlighted" : ""
-        }`}
-      >
-        <p className="item-score">4 звезды</p>
-        <div className="progress-bar">
-          <p className="item-value">{rating4} отзывов</p>
-          <div
-            className="progress"
-            style={{ width: `${calculatePercentage(rating4, totalReviews)}%` }}
-          ></div>
-        </div>
-      </div>
-
-      <div
-        className={`reviews-score__item ${
-          rating3 === maxRating && !isHovered ? "highlighted" : ""
-        }`}
-      >
-        <p className="item-score">3 звезды</p>
-        <div className="progress-bar">
-          <p className="item-value">{rating3} отзывов</p>
-          <div
-            className="progress"
-            style={{ width: `${calculatePercentage(rating3, totalReviews)}%` }}
-          ></div>
-        </div>
-      </div>
-
-      <div
-        className={`reviews-score__item ${
-          rating2 === maxRating && !isHovered ? "highlighted" : ""
-        }`}
-      >
-        <p className="item-score">2 звезды</p>
-        <div className="progress-bar">
-          <p className="item-value">{rating2} отзывов</p>
-          <div
-            className="progress"
-            style={{ width: `${calculatePercentage(rating2, totalReviews)}%` }}
-          ></div>
-        </div>
-      </div>
-
-      <div
-        className={`reviews-score__item ${
-          rating1 === maxRating && !isHovered ? "highlighted" : ""
-        }`}
-      >
-        <p className="item-score">1 звезда</p>
-        <div className="progress-bar">
-          <p className="item-value">{rating1} отзывов</p>
-          <div
-            className="progress"
-            style={{ width: `${calculatePercentage(rating1, totalReviews)}%` }}
-          ></div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
