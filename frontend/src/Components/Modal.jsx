20c99f208e4c87fb725d4ea2b57a8e593cd71b19
Ivan Trifonov <tr1one@ya.ru>
2025-03-16 22:36:06 +0300

    use keyboard in Modal

--- frontend/src/Components/Modal.jsx
+++ frontend/src/Components/Modal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 
 const Modal = ({
   isOpen,
@@ -10,6 +10,33 @@ const Modal = ({
   onButtonClick = onClose,
   onSecondaryClick = onClose,
 }) => {
+  // Мемоизация функции handleKeyDown с помощью useCallback
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.key === "Escape") {
+        onClose(); // Закрыть модальное окно при нажатии Escape
+      }
+      if (event.key === "Enter" && isOpen) {
+        onButtonClick(); // Выполнить основное действие при нажатии Enter
+      }
+    },
+    [isOpen, onClose, onButtonClick]
+  );
+
+  // Добавляем слушатель событий клавиатуры при монтировании компонента
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+    }
+
+    // Удаляем слушатель событий при размонтировании или закрытии модального окна
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, handleKeyDown]); // Теперь handleKeyDown включен в массив зависимостей
+
+  if (!isOpen) return null;
+
   return (
     <div className={`modal-overlay ${isOpen ? "active" : ""}`}>
       <div className="modal">
